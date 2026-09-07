366ddc699476dd5ce3297c58b651fbc828a91a15
Azimn <jratican@gmail.com>
2026-09-07 14:31:05 -0500

    Make v0.8 agency simulation failures report gate names

--- tests/test_agency_simulation_v08.py
+++ tests/test_agency_simulation_v08.py
@@ -3,7 +3,7 @@ from duck.agency_simulation import run_agency_simulation
 
 def test_long_horizon_v08_agency_simulation_passes_all_gates():
     report = run_agency_simulation()
-    assert report["passed"], report
     assert all(report["gates"].values()), report["gates"]
+    assert report["passed"]
     assert report["final"]["subject_id"] == "agency-14d"
     assert not report["final"]["open_concerns"]
